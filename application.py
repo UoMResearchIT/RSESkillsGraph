693cd4491cbe8dbb1841c477add04a12932cf5be
Ian Hinder <ian.hinder@manchester.ac.uk>
2025-04-29 16:21:40 +0100

    application.py: Remove explicit error handling
    In the old code, any exception was caught and replaced with a print
    statement followed by exiting the program. For a web app, we probably
    don't want to exit the program. Instead, we will let the exception be
    handled by the web app runner. This allows the flask debugger, for example,
    to be used.

--- application.py
+++ application.py
@@ -60,13 +60,9 @@ def get_people():
     url = 'https://balextest.itservices.manchester.ac.uk/api/skills/getAllGrouped'
     api_key = os.getenv('CAPX_API_KEY')
     headers = {'x-api-key': api_key}
-    try:
-        request = urllib.request.Request(url, headers=headers)
-        response = urllib.request.urlopen(request)
-        data = json.loads(response.read())
-    except Exception as e:
-        print(f'Error fetching data from API: {e}', file=sys.stderr)
-        sys.exit(1)
+    request = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(request)
+    data = json.loads(response.read())
     
     # Transform the data to match the old format
     transformed_data = {}
