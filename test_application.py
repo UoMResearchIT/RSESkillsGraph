38d7f744899f93a8949aa5b7c7bd5e205ef26067
Ian Hinder <ian.hinder@manchester.ac.uk>
2025-04-30 23:34:30 +0100

    test_application.py: Remove leftover mocker

--- test_application.py
+++ test_application.py
@@ -3,7 +3,7 @@
 import application
 
 # This test should be run against the capxmock service
-def test_get_people(mocker):
+def test_get_people():
     people_result = {'Person A': {'interests': ['Skill 1', 'Skill 2']}, 'Person B': {'interests': ['Skill 3']}}
     people = application.get_people()
     assert(people == people_result)
